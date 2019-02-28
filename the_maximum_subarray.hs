86f0d2cbb07272bc9f34b1a7ea8bec86efdc8ff3
Maximiliano Romay Figueroa <maximilianorom7@gmail.com>
2019-02-28 20:21:05 -0300

    changed the comment header of the maximum subarray exercise

--- the_maximum_subarray.hs
+++ the_maximum_subarray.hs
@@ -1,3 +1,14 @@
--- Solution for:
--- https://www.hackerrank.com/challenges/maxsubarray/problem
+{-
 
+Solution for:
+https://www.hackerrank.com/challenges/maxsubarray/problem
+
+We define subsequence as any subset of an array. We define a subarray as a contiguous subsequence in an array.
+
+Given an array, find the maximum possible sum among:
+
+all nonempty subarrays.
+all nonempty subsequences.
+Print the two values as space-separated integers on one line.
+
+-}
